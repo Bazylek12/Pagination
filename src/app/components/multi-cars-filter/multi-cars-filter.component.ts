fe5ce2fc0ba0e5b7646e6b20b2250e8076763284
Bazylek12 <a.kosmowski7@gmail.com>
2023-01-11 21:26:56 +0100

    feat: multi cars filter | working brand filtering

--- src/app/components/multi-cars-filter/multi-cars-filter.component.ts
+++ src/app/components/multi-cars-filter/multi-cars-filter.component.ts
@@ -1,16 +1,17 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {combineLatest, Observable, take} from 'rxjs';
-import { BrandModel } from '../../models/brand.model';
-import { ComfortFeatureModel } from '../../models/comfort-feature.model';
-import { CarModel } from '../../models/car.model';
-import { CarsService } from '../../services/cars.service';
+import {BrandModel} from '../../models/brand.model';
+import {ComfortFeatureModel} from '../../models/comfort-feature.model';
+import {CarModel} from '../../models/car.model';
+import {CarsService} from '../../services/cars.service';
 import {map, tap} from "rxjs/operators";
 
 interface filteredData {
   brands: Set<string>,
   comfortFeatures: Set<string>,
 }
+
 @Component({
   selector: 'app-multi-cars-filter',
   templateUrl: './multi-cars-filter.component.html',
@@ -21,9 +22,9 @@ export class MultiCarsFilterComponent {
   readonly brandsList$: Observable<BrandModel[]> = this._carsService.getBrands();
   readonly comfortFeaturesList$: Observable<ComfortFeatureModel[]> = this._carsService.getComfortFeatures();
   readonly filteredList$: Observable<filteredData> = this._activatedRoute.queryParams.pipe(
-    map((params) : filteredData => ({
-        brands: new Set<string>(params['brands'] === undefined ? [] :  params['brands'].split(',') ),
-        comfortFeatures: new Set<string>(params['comfort-features'] === undefined ? [] : params['comfort-features'].split(','))
+    map((params): filteredData => ({
+      brands: new Set<string>(params['brands'] === undefined ? [] : params['brands'].split(',')),
+      comfortFeatures: new Set<string>(params['comfort-features'] === undefined ? [] : params['comfort-features'].split(','))
     }))
   );
   readonly carsList$: Observable<CarModel[]> = combineLatest([
@@ -31,9 +32,11 @@ export class MultiCarsFilterComponent {
     this.filteredList$
   ]).pipe(
     map(([cars, list]) =>
-      cars.filter(car => list.brands.size === 0 || list.brands.has(car.brandId)
-      ).filter(car => list.comfortFeatures.size === 0 || [car.comfortFeatureIds].filter((id) => list.comfortFeatures.has(id))
-    ))
+      cars
+        .filter(car => list.brands.size === 0 || list.brands.has(car.brandId))
+        // .filter(car => list.comfortFeatures.size === 0 ||
+        //   car.comfortFeatureIds.filter((id: string) => list.comfortFeatures.has(id).length === list.comfortFeatures.size))
+    )
   )
 
   onBrandSelected(brand: BrandModel, isSelected: boolean) {
@@ -44,12 +47,13 @@ export class MultiCarsFilterComponent {
         isSelected ? brandParam.add(brand.id) : brandParam.delete(brand.id);
         this._router.navigate([], {
           queryParams: {
-            brands: [...brandParam].join(',')
+            brands: brandParam.size === 0 ? brandParam.clear() : [...brandParam].join(',')
           }
         })
       })
     ).subscribe()
   }
+
   constructor(private _carsService: CarsService, private _activatedRoute: ActivatedRoute, private _router: Router) {
   }
 
